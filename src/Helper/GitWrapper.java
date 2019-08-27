e14efa4371af2ed42655eaa4ea0f4625a0272217
mbrebion <maxence.miguelbrebion@gmail.com>
2019-08-27 19:47:54 +0200

    git add . instead of git dd exercice/ before commit

--- src/Helper/GitWrapper.java
+++ src/Helper/GitWrapper.java
@@ -21,7 +21,7 @@ public class GitWrapper {
 
     public static String commit() {
         String commitMessage = "\"" + JOptionPane.showInputDialog("commentaire du commit") + "\"";
-        String output = ExecCommand.execo(new String[]{"git", "add", "exercices"}, SavedVariables.getMainGitDir())[1];
+        String output = ExecCommand.execo(new String[]{"git", "add", "."}, SavedVariables.getMainGitDir())[1];
         output += " \n  " + ExecCommand.execo(new String[]{"git", "commit", "-a", "-m", commitMessage}, SavedVariables.getMainGitDir())[1];
         //JOptionPane.showMessageDialog(null, output);
         return output;
